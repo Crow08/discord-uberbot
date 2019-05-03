e96e78ae5bcb3c2ec25fa528c720a7644f25d63e
Kevin Hertfelder <kevin.game@web.de>
2019-05-03 18:52:26 +0200

    Added no_dc argument and readline support.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const settings = require("../settings.json");
 const Discord = require("discord.js");
 const MusicClient = require("./MusicClient.js");
+const readline = require("readline");
 
 const baseClient = new Discord.Client();
 const musicClient = new MusicClient(baseClient, Discord, {
@@ -12,7 +13,9 @@ const musicClient = new MusicClient(baseClient, Discord, {
   "spotifyClientSecret": settings.spotifyClientSecret
 });
 
-baseClient.login(settings.token);
+if (!process.argv.includes("no_dc")) {
+  baseClient.login(settings.token);
+}
 
 baseClient.on("message", (msg) => {
   if (msg.author.bot && !settings.botTalk) {
@@ -20,7 +23,7 @@ baseClient.on("message", (msg) => {
   }
   msg.content.split("\n").forEach((element) => {
     const message = element.trim();
-    if (message.startsWith(settings.botPrefix) && msg.channel.type === "text") {
+    if (message.startsWith(settings.botPrefix) && (msg.channel.type === "text" || msg.channel.type === "dm")) {
       const cmd = message.substr(settings.botPrefix.length).split(" ", 1)[0];
       const payload = message.substr(cmd.length + settings.botPrefix.length + 1);
       musicClient.execute(cmd, payload, msg);
@@ -30,4 +33,20 @@ baseClient.on("message", (msg) => {
 
 baseClient.on("ready", () => {
   console.log("------- UberBot is fully charged! -------\n>");
+});
+
+// DEBUG STUFF:
+
+const rl = readline.createInterface({
+  "input": process.stdin,
+  "output": process.stdout
+});
+
+rl.on("line", (input) => {
+  const message = input.trim();
+  if (message.startsWith(settings.botPrefix)) {
+    const cmd = message.substr(settings.botPrefix.length).split(" ", 1)[0];
+    const payload = message.substr(cmd.length + settings.botPrefix.length + 1);
+    musicClient.execute(cmd, payload);
+  }
 });
