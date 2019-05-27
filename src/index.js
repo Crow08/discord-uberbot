da82d131f5855351f97dcd196fc3a56092c15322
Kevin Hertfelder <kevin.game@web.de>
2019-05-27 16:36:47 +0200

    Added bot_only and web_only flags to cla and env. Added heroku procfile.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const readline = require("readline");
 const http = require("http");
 const fs = require("fs");
 
-console.log("\x1b[32m%s\x1b[0m", "--------    UberBot is charging!    --------\n");
-
 let debug = false;
+let webOnly = false;
+let botOnly = false;
 let settingsPath = "./settings.json";
 let settingsUrl = "";
 
@@ -18,6 +18,10 @@ let musicClient = null;
 process.argv.forEach((arg) => {
   if (arg === "debug") {
     debug = true;
+  } else if (arg === "web_only") {
+    webOnly = true;
+  } else if (arg === "bot_only") {
+    botOnly = true;
   } else if (arg.match(/^settings_url=.+/u)) {
     settingsUrl = arg.split("=")[1];
   } else if (arg.match(/^settings_path=.+/u)) {
@@ -28,6 +32,12 @@ process.argv.forEach((arg) => {
 // Parsing environment variables.
 if (debug === false && typeof process.env.debug !== "undefined") {
   debug = typeof process.env.debug;
+}
+if (webOnly === false && typeof process.env.web_only !== "undefined") {
+  webOnly = typeof process.env.web_only;
+}
+if (botOnly === false && typeof process.env.bot_only !== "undefined") {
+  botOnly = typeof process.env.bot_only;
 }
 if (settingsUrl === "" && typeof process.env.settings_url !== "undefined") {
   settingsUrl = process.env.settings_url;
@@ -49,7 +59,7 @@ const loadSettings = () => new Promise((resolve, reject) => {
         }
         resolve(JSON.parse(rawData));
       });
-    // If settingsUrl is set:
+      // If settingsUrl is set:
     } else if (settingsUrl.length > 0) {
       // Download settings file.
       http.get(settingsUrl, (response) => {
@@ -66,9 +76,9 @@ const loadSettings = () => new Promise((resolve, reject) => {
       });
     } else {
       reject(new Error("Failed to load settings file!\n" +
-      "Please provide a settings file at the default location (\"./settings.json\") " +
-      "or set a path through the \"settings_path\" argument.\n" +
-      "Alternatively you can provide \"settings_url\" as argument to refer a remote settings file."));
+        "Please provide a settings file at the default location (\"./settings.json\") " +
+        "or set a path through the \"settings_path\" argument.\n" +
+        "Alternatively you can provide \"settings_url\" as argument to refer a remote settings file."));
     }
   });
 });
@@ -159,52 +169,61 @@ const setConsoleEventListeners = () => {
   });
 };
 
+// Fill in missing settings with default values and validate settings.
+const validateSettings = (() => {
+  // Set default values if necessary.
+  settings.bitRat = typeof settings.bitRate === "undefined" ? 96000 : parseInt(settings.bitRate, 10);
+  settings.botPrefix = typeof settings.botPrefix === "undefined" ? "!" : settings.botPrefix;
+  settings.defVolume = typeof settings.defVolume === "undefined" ? 50 : parseInt(settings.defVolume, 10);
+  settings.ratingCooldown = typeof settings.ratingCooldown === "undefined" ? 86400
+    : parseInt(settings.ratingCooldown, 10);
+  // Check for missing credentials.
+  return ((typeof settings.scClientId !== "undefined" && settings.scClientId !== "SOUNDCLOUD_CLIENT_ID") ||
+    (typeof settings.spotifyClientId !== "undefined" && settings.spotifyClientId !== "SPOTIFY_CLIENT_ID") ||
+    (typeof settings.spotifyClientSecret !== "undefined" && settings.spotifyClientSecret !== "SPOTIFY_CLIENT_SECRET") ||
+    (typeof settings.youtubeApiKey !== "undefined" && settings.youtubeApiKey !== "YOUTUBE_API_KEY"));
+});
+
 // Script starts here:
-loadSettings().then((json) => {
-  settings = json;
-  // Check if all necessary settings are set.
-  if ((typeof settings.scClientId === "undefined" || settings.scClientId === "SOUNDCLOUD_CLIENT_ID") ||
-    (typeof settings.spotifyClientId === "undefined" || settings.spotifyClientId === "SPOTIFY_CLIENT_ID") ||
-    (typeof settings.spotifyClientSecret === "undefined" || settings.spotifyClientSecret === "SPOTIFY_CLIENT_SECRET") ||
-    (typeof settings.youtubeApiKey === "undefined" || settings.youtubeApiKey === "YOUTUBE_API_KEY")) {
-    console.log("\x1b[31m%s\x1b[0m", new Error("Your setting file is missing some necessary information!\n" +
-    "Please check your settings file for missing API credentials.\n" +
-    "Take a look at settings.example.json for reference."));
-    return;
-  }
+if (!webOnly) {
+  console.log("\x1b[32m%s\x1b[0m", "--------    UberBot is charging!    --------\n");
+  loadSettings().then((json) => {
+    settings = json;
+    // Check if all necessary settings are set.
+    if (!validateSettings()) {
+      console.log("\x1b[31m%s\x1b[0m", new Error("Your setting file is missing some necessary information!\n" +
+        "Please check your settings file for missing API credentials.\n" +
+        "Take a look at settings.example.json for reference."));
+      return;
+    }
 
-  // Create base client (discord.js client) and music client (uberbot client).
-  // These provide the main functionality of the bot.
-  baseClient = new Discord.Client();
-  musicClient = new MusicClient(baseClient, Discord.MessageEmbed, {
-    "bitRate": typeof settings.bitRate === "undefined" ? 96000 : parseInt(settings.bitRate, 10),
-    "botPrefix": typeof settings.botPrefix === "undefined" ? "!" : settings.botPrefix,
-    "defVolume": typeof settings.defVolume === "undefined" ? 50 : parseInt(settings.defVolume, 10),
-    "mongodbPassword": settings.mongodbPassword,
-    "mongodbUrl": settings.mongodbUrl,
-    "mongodbUser": settings.mongodbUser,
-    "ratingCooldown": typeof settings.ratingCooldown === "undefined" ? 86400 : parseInt(settings.ratingCooldown, 10),
-    "scClientId": settings.scClientId,
-    "spotifyClientId": settings.spotifyClientId,
-    "spotifyClientSecret": settings.spotifyClientSecret,
-    "youtubeApiKey": settings.youtubeApiKey
-  });
+    // Create base client (discord.js client) and music client (uberbot client).
+    // These provide the main functionality of the bot.
+    baseClient = new Discord.Client();
+    musicClient = new MusicClient(baseClient, Discord.MessageEmbed, settings);
 
-  // Set up event listeners for Discord events like incoming messages.
-  setBotEventListeners();
+    // Set up event listeners for Discord events like incoming messages.
+    setBotEventListeners();
 
-  // Setup console listeners for debugging purposes.
-  setConsoleEventListeners();
+    // Setup console listeners for debugging purposes.
+    setConsoleEventListeners();
 
-  // Login to Discord.
-  baseClient.login(settings.token).
-    catch((err) => console.log(err));
-}).
-  catch((err) => {
-    console.log("\x1b[31m%s\x1b[0m", err);
-  });
+    // Login to Discord.
+    baseClient.login(settings.token).
+      catch((err) => console.log(err));
+  }).
+    catch((err) => {
+      console.log("\x1b[31m%s\x1b[0m", err);
+    });
+}
 
-http.createServer((request, response) => {
-  response.write("Anyone called for a medic?");
-  response.end();
-}).listen(process.env.PORT || 8080);
+if (!botOnly) {
+  console.log("\x1b[34m%s\x1b[0m", "--------    WebServer is starting!    --------\n");
+  http.createServer((request, response) => {
+    response.write("Anyone called for a medic?");
+    response.end();
+  }).listen(
+    process.env.PORT || 8080,
+    () => console.log("\x1b[34m%s\x1b[0m", "--------    WebServer successfully started!    --------\n")
+  );
+}
