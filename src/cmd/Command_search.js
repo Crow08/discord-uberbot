2091aaf4dc7b55d03969e5c58d2c1c42aef32965
Kevin Hertfelder <kevin.game@web.de>
2022-10-01 23:24:22 +0200

    reduce scope of search command

--- src/cmd/Command_search.js
+++ src/cmd/Command_search.js
@@ -1,82 +1,7 @@
-// TODO: Probably doesn't work anymore
 const chatService = require("../ChatService");
 const searchService = require("../SearchService");
-const dbService = require("../DBService");
-const queueService = require("../QueueService");
-const playerService = require("../PlayerService");
-const {SlashCommandBuilder,
-  EmbedBuilder
-} = require("discord.js");
+const {SlashCommandBuilder, EmbedBuilder} = require("discord.js");
 
-/**
- * Filter for incoming messages for await command.
- * @private
- * @param {Message} resp Collected User responses.
- */
-const isSelectionCmd = (resp) => {
-  const message = resp.content.trim().
-    toLowerCase();
-  if ((!isNaN(message) && message.length > 0) || message === "cancel") {
-    return true;
-  }
-  const cmd = message.split(" ")[0];
-  if ((message.split(" ").length === 2 && ["a", "add", "p", "play"].includes(cmd) && !isNaN(message.split(" ")[1])) ||
-    (message.split(" ").length === 3 && ["pla", "pladd"].includes(cmd) && !isNaN(message.split(" ")[2]))) {
-    return true;
-  }
-  return false;
-};
-
-/**
- * Process followup command for user selection by adding or playing the song.
- * @private
- * @param {Message[]} collected Collected User responses in this case always a single element.
- * @param {*} songs songs in the selection.
- */
-const processSelectionCmd = (collected, songs) => {
-  const response = collected.array()[0];
-  const content = response.content.trim().
-    toLowerCase();
-  if (!isNaN(content)) {
-    playerService.playNow(songs[content - 1], response);
-  } else if (content === "cancel") {
-    chatService.simpleNote(response, "Selection canceled!", chatService.msgType.MUSIC);
-  } else {
-    const cmd = content.split(" ")[0];
-    let song = null;
-    let plName = null;
-    if (content.split(" ").length === 2) {
-      song = songs[content.split(" ")[1] - 1];
-    } else {
-      plName = content.split(" ")[1];
-      song = songs[content.split(" ")[2] - 1];
-    }
-
-    switch (cmd) {
-    case "p":
-    case "play": {
-      playerService.playNow(song, response);
-      break;
-    }
-    case "a":
-    case "add": {
-      queueService.addFairlyToQueue(song);
-      const note = `Song added to queue: ${song.title}`;
-      chatService.simpleNote(response, note, chatService.msgType.MUSIC);
-      break;
-    }
-    case "pla":
-    case "pladd": {
-      dbService.addSong(song, plName);
-      const note = `Song added ${song.title} to playlist ${plName}`;
-      chatService.simpleNote(response, note, chatService.msgType.MUSIC);
-      break;
-    }
-    default:
-      break;
-    }
-  }
-};
 
 const run = (interaction) => {
   const songQuery = interaction.options.getString("song_query");
@@ -119,13 +44,7 @@ const run = (interaction) => {
         chatService.simpleNote(interaction, "No search results!", chatService.msgType.INFO, true);
       } else {
         chatService.simpleNote(interaction, "Searching for song:", chatService.msgType.MUSIC, true);
-        chatService.pagedContent(interaction, pages).
-          then((pagedMsg) => chatService.awaitCommand(
-            interaction, (resp) => isSelectionCmd(resp), (col) => processSelectionCmd(col, eSongs),
-            () => {
-              pagedMsg.delete();
-            }
-          ));
+        chatService.pagedContent(interaction, pages);
       }
     }).
     catch((error) => chatService.simpleNote(interaction, error, chatService.msgType.FAIL, true));
@@ -134,7 +53,7 @@ const run = (interaction) => {
 module.exports = {
   "data": new SlashCommandBuilder().
     setName("search").
-    setDescription("Search for a song and choose from multiple results.").
+    setDescription("Search for a song and get a list of up to 50 results.").
     addStringOption((option) => option.
       setName("song_query").
       setDescription("SearchTerm for song").
